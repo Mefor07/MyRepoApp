8bcecc2bbabdc63c224c4959b11c9db4ff7341b5
Mefor07 <mefor.louis@gmail.com>
2020-11-07 07:24:38 +0000

    Added table view structure without story boards

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyRepoApp
+//
+//  Created by Mefor Louis on 11/6/20.
+//  Copyright © 2020 Pmedialabs. All rights reserved.
+//
+
+import UIKit
+
+class RepoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
